f77972de441e86e34e38ae85c04519a0728c6a3a
Vinicius <viniciuz6460@gmail.com>
2021-04-30 17:35:32 -0300

    adicionando tabela no banco

--- minha_primeira_api/routes/api.php
+++ minha_primeira_api/routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function(Request $request){
+    // dd($request->headers->all());
+    // dd($request->headers->get('Authorization'));
+
+    $response = new \Illuminate\Http\Response(json_encode(['msg'=>'Minha primeira respost de API']));
+    $response->header('Content-Type', 'application/json');
+
+    return $response;
+});
